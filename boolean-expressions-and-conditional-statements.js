a3448b10031e6ca67f4977c764b5298f8af613e1
lakshmi priya <lakshmipriyaark@gmail.com>
2026-04-20 11:35:19 -0500

    Prework boolean expression coding added

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -28,19 +28,61 @@ const readline = require('readline-sync');
 
 const hasTorch = true;
 const hasMap = false;
+const huntingSword = false;
+const hasHangers = true;
+const hasRope = false;
 
 console.log("You see two paths: one leads to the mountains, the other to the village.");
 const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
 
-if (choice === "mountains" && hasTorch) {
+if (choice === "mountains" && hasTorch) 
+  {
   console.log("You safely navigate through the dark mountains.");
-} else if (choice === "mountains" && !hasTorch) {
+  const choice2 = readline.question("Do you want huntingsword ? say \" yes \" or \" No \" ");
+  
+  if ( choice2 === "yes" && hasTorch)
+  {
+    console.log ("Get the hunting sword cave 2 ");
+  }
+    else if (choice2 === "no" && hasHangers){
+    console.log("Okay.You have hangers so you can safe to go.Continue your way");
+    }
+
+  else    //It will run both are false .so it can't
+  {
+    console.log("Be careful animals will attack you");
+  }
+  
+  const choice3 = readline.question("Do you need Compass? ");
+  
+  if (choice3 === "yes")
+    { console.log ("Get it from shelter in entrance");
+  }
+  else {
+    console.log("You continue without a compass.");
+  }
+   const choice4 = readline.question("Do you want Rope? say \" yes \" or \" no \" ");
+   if (choice4 === "yes" && hasHangers){
+    console.log ("Get the rope in Cave 5.Your are in safe");}
+    else if (choice4 === "no" && hasHangers){
+    console.log("You don\'t have rope. Be careful . Some places are slippery ");}
+    else 
+    {
+      console.log ("You don\'t have hangers and rope.Your in dangeous");
+    }
+  }
+
+else if (choice === "mountains" && !hasTorch) 
+  {
   console.log("It's too dark to proceed. You decide to turn back.");
-} else if (choice === "village" || hasMap) {
+}
+ else if (choice === "village" || hasMap) {
   console.log("You find your way to the village.");
-} else {
+} 
+else {
   console.log("You get lost and wander aimlessly.");
 }
+  
 
 /* 
 
